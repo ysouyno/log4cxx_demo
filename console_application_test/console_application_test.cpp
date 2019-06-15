83ac9e170e42b7e27afce183fb318b98cffdfce6
ysouyno <ysouyno@163.com>
2019-06-15 17:04:19 +0800

    test fine in console application, but no Chinese output in ConsoleAppender

--- console_application_test/console_application_test.cpp
+++ console_application_test/console_application_test.cpp
@@ -2,9 +2,25 @@
 //
 
 #include "pch.h"
-#include <iostream>
+#include <Windows.h>
+#include <atlstr.h>
+#include <string>
+#include <log4cxx/logger.h>
+
+log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("console"));
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    LOG4CXX_INFO(logger, "main");
+
+    CString cstr(L"cstring");
+    LOG4CXX_INFO(logger, cstr.GetBuffer()); // need .GetBuffer()
+
+    cstr = L"ÖÐÎÄ";
+    LOG4CXX_INFO(logger, cstr.GetBuffer());
+
+    std::string str("string");
+    LOG4CXX_INFO(logger, str);
+
+    return 0;
 }
